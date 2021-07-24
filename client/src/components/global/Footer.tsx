6b09977c0011788263ee090aeb6514f06f0aeee8
Ranjeet Singh <ranjeetsingheminence@gmail.com>
2021-07-24 10:54:09 +0530

    complete auth with env

--- client/src/components/global/Footer.tsx
+++ client/src/components/global/Footer.tsx
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <div className="text-center bg-light py-4">
       <h6>Welcome ITJ Jodhpur</h6>
-      <a href="https://www.youtube.com/c/DevATHTML" 
+      <a href="https://www.youtube.com/channel/UC9xhwQ-x0gnIy-GJWSB6Itw" 
       target="_blank" rel="noreferrer"
       className="mb-2 d-block">
-https://www.youtube.com/channel/UC9xhwQ-x0gnIy-GJWSB6Itw      </a>
+https://www.youtube.com/channel/UC9xhwQ-x0gnIy-GJWSB6Itw   </a>
       <p> Copyright &copy; 2021</p>
     </div>
   )
